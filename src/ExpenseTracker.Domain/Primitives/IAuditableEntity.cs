1a7c737d430131b1e324ec42693a04274e6172cb
Milan Jovanović <milannis.995@gmail.com>
2019-07-20 22:28:30 +0200

    - Added IAuditableEntity interface for easier tracking of entity lifetime

---
+++ src/ExpenseTracker.Domain/Primitives/IAuditableEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExpenseTracker.Domain.Primitives
+{
+    /// <summary>
+    /// Represents a marker interface for an auditable entity.
+    /// </summary>
+    public interface IAuditableEntity
+    {
+        /// <summary>
+        /// Gets the created on date and time in UTC format.
+        /// </summary>
+        DateTime CreatedOnUtc { get; }
+
+        /// <summary>
+        /// Gets the updated on date and time in UTC format.
+        /// </summary>
+        DateTime? UpdatedOnUtc { get; }
+    }
+}
