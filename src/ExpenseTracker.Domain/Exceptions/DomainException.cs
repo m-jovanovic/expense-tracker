69bdb0ce76964dbe3f2a9c9983fcae904f1a8222
Milan Jovanović <milannis.995@gmail.com>
2019-07-20 12:52:00 +0200

    - Added DomainException for representing exceptions that occur in the domain

---
+++ src/ExpenseTracker.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExpenseTracker.Domain.Exceptions
+{
+    /// <summary>
+    /// Represents an error that occurs in the domain layer.
+    /// </summary>
+    public class DomainException : Exception
+    {
+        /// <inheritdoc />
+        public DomainException()
+        {
+        }
+
+        /// <inheritdoc />
+        public DomainException(string message) : base(message)
+        {
+        }
+
+        /// <inheritdoc />
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
