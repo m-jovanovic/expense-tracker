f1084d090f6fc58da2f1bec438dd17146a381baa
Milan Jovanović <milannis.995@gmail.com>
2019-07-20 12:50:06 +0200

    - Added AggregateRoot class and IDomainEvent interface for representing domain events using MediatR

---
+++ src/ExpenseTracker.Domain/Events/IDomainEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace ExpenseTracker.Domain.Events
+{
+    /// <summary>
+    /// Represents a domain event handler interface.
+    /// </summary>
+    /// <typeparam name="TDomainEvent">The domain event type.</typeparam>
+    public interface IDomainEventHandler<in TDomainEvent> : INotificationHandler<TDomainEvent> where TDomainEvent : IDomainEvent
+    {
+    }
+}
