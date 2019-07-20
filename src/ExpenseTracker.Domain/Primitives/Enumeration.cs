e5779d15e40c4e645e93bbf8db6ebf13ce48cf84
Milan Jovanović <milannis.995@gmail.com>
2019-07-20 22:14:54 +0200

    - Added enumeration class for representing enumerations with additional behavior

---
+++ src/ExpenseTracker.Domain/Primitives/Enumeration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ExpenseTracker.Domain.Primitives
+{
+    /// <summary>
+    /// Represents an enumeration type.
+    /// </summary>
+    public abstract class Enumeration : IComparable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Enumeration"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="name">The name.</param>
+        protected Enumeration(int value, string name)
+        {
+            Value = value;
+            Name = name;
+        }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        public int Value { get; }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <inheritdoc />
+        public int CompareTo(object other)
+        {
+            return Value.CompareTo(((Enumeration)other).Value);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Enumeration otherValue))
+            {
+                return false;
+            }
+
+            return GetType() == obj.GetType() && otherValue.Value.Equals(Value);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
