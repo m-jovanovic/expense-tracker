35006038aaeb013a9d5247ebf432d4fb3023f0ad
Milan Jovanović <milannis.995@gmail.com>
2019-07-20 12:51:33 +0200

    - Added IUnitOfWork and IRepository interfaces

---
+++ src/ExpenseTracker.Domain/Abstractions/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using ExpenseTracker.Domain.Primitives;
+
+namespace ExpenseTracker.Domain.Abstractions
+{
+    /// <summary>
+    /// Represents a repository interface.
+    /// </summary>
+    /// <typeparam name="TAggregateRoot">The aggregate root type.</typeparam>
+    public interface IRepository<TAggregateRoot> where TAggregateRoot : AggregateRoot
+    {
+        /// <summary>
+        /// Gets the aggregate root with the specified identifier if it exists.
+        /// </summary>
+        /// <param name="id">The aggregate root identifier.</param>
+        /// <returns>The aggregate root instance with the specified identifier if it exists, otherwise no value.</returns>
+        Task<Maybe<TAggregateRoot>> GetByIdAsync(Guid id);
+    }
+}
---
+++ src/ExpenseTracker.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Domain.Abstractions
+{
+    /// <summary>
+    /// Represents the interface for a unit of work.
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Saves all of the changes in the unit of work.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The number of entities that were saved.</returns>
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
