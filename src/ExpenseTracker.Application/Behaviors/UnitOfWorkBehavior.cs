1ff45d6eb336dadba1deea3f3c500f428670120c
Milan Jovanović <milannis.995@gmail.com>
2019-07-21 21:51:05 +0200

    - Implemented UnitOfWorkBehavior that will complete a unit of work after each request

---
+++ src/ExpenseTracker.Application/Behaviors/UnitOfWorkBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ExpenseTracker.Domain.Abstractions;
+using MediatR;
+
+namespace ExpenseTracker.Application.Behaviors
+{
+    public class UnitOfWorkBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UnitOfWorkBehavior(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            TResponse response = await next();
+
+            // Only use the unit of work if the current request is a command.
+            // There is no need to save changes in any other case.
+            if (request.GetType().Name.EndsWith("Command"))
+            {
+                await _unitOfWork.SaveChangesAsync(cancellationToken);
+            }
+
+            return response;
+        }
+    }
+}
