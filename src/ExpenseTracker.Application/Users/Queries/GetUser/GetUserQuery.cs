a6127f7d5625cc7966d843ba0d375a3214a67416
Milan Jovanović <milannis.995@gmail.com>
2019-07-21 21:50:35 +0200

    - Added Dapper and implemented GetUserQuery and GetUserQueryHandler using dapper

---
+++ src/ExpenseTracker.Application/Users/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ExpenseTracker.Domain.Aggregates.UserAggregate;
+using ExpenseTracker.Domain.Primitives;
+using MediatR;
+
+namespace ExpenseTracker.Application.Users.Queries.GetUser
+{
+    public class GetUserQuery : IRequest<Maybe<User>>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/ExpenseTracker.Application/Users/Queries/GetUser/GetUserQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using ExpenseTracker.Application.Infrastructure;
+using ExpenseTracker.Domain.Aggregates.UserAggregate;
+using ExpenseTracker.Domain.Primitives;
+using MediatR;
+
+namespace ExpenseTracker.Application.Users.Queries.GetUser
+{
+    public class  GetUserQueryHandler : IRequestHandler<GetUserQuery, Maybe<User>>
+    {
+        private readonly ConnectionString _connectionString;
+
+        public GetUserQueryHandler(ConnectionString connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<Maybe<User>> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            if (request.Id == Guid.Empty)
+            {
+                return null;
+            }
+
+            const string sql = "SELECT * FROM [User] WHERE Id = @Id";
+
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                return await sqlConnection.QuerySingleOrDefaultAsync<User>(sql, request);
+            }
+        }
+    }
+}
