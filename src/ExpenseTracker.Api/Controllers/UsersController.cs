7c45d6a285e95b1708b9ae92fb41e7b6d1aeb034
Milan Jovanović <milannis.995@gmail.com>
2019-07-21 21:53:13 +0200

    - Added Api web application to serve as a REST API for client application, implemented GetUser and CreateUser methods in UsersController

---
+++ src/ExpenseTracker.Api/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using ExpenseTracker.Application.Users.Commands;
+using ExpenseTracker.Application.Users.Commands.CreateUser;
+using ExpenseTracker.Application.Users.Queries.GetUser;
+using ExpenseTracker.Domain.Aggregates.UserAggregate;
+using ExpenseTracker.Domain.Primitives;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(Guid id)
+        {
+            Maybe<User> userOrNothing = await _mediator.Send(new GetUserQuery { Id = id });
+
+            if (userOrNothing.HasNoValue)
+            {
+                return NotFound();
+            }
+
+            return Ok(userOrNothing.Value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserCommand createUserCommand)
+        {
+            Result result = await _mediator.Send(createUserCommand);
+
+            if (result.IsFailure)
+            {
+                return BadRequest(result.Error);
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ src/ExpenseTracker.Api/Startup.cs
@@ -0,0 +1,73 @@
+﻿using ExpenseTracker.Application.Abstractions;
+using ExpenseTracker.Application.Behaviors;
+using ExpenseTracker.Application.Infrastructure;
+using ExpenseTracker.Application.Users.Commands;
+using ExpenseTracker.Application.Users.Commands.CreateUser;
+using ExpenseTracker.Domain.Abstractions;
+using ExpenseTracker.Domain.Aggregates.ExpenseAggregate;
+using ExpenseTracker.Domain.Aggregates.UserAggregate;
+using ExpenseTracker.Infrastructure;
+using ExpenseTracker.Infrastructure.Repository;
+using ExpenseTracker.Persistence;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: ApiController]
+namespace ExpenseTracker.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = new ConnectionString(Configuration["ConnectionString"]);
+
+            services.AddSingleton(connectionString);
+
+            services.AddDbContext<ExpenseTrackerDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddMediatR(typeof(CreateUserCommand));
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(UnitOfWorkBehavior<,>));
+
+            services.AddTransient<IDateTime, MachineDateTime>();
+
+            services.AddScoped<IDbContext>(factory => factory.GetRequiredService<ExpenseTrackerDbContext>());
+            services.AddScoped<IUnitOfWork>(factory => factory.GetRequiredService<ExpenseTrackerDbContext>());
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IExpenseRepository, ExpenseRepository>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseMvc();
+        }
+    }
+}
