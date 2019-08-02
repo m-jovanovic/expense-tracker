82da0581057b43fcfdf68cef76f35ab498cbfac6
Milan Jovanović <milannis.995@gmail.com>
2019-08-02 22:45:09 +0200

    - Added tests for Money value object

---
+++ src/ExpenseTracker.Domain.Tests/ValueObjects/MoneyTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using ExpenseTracker.Domain.Aggregates.ExpenseAggregate;
+using Shouldly;
+using Xunit;
+
+namespace ExpenseTracker.Domain.Tests.ValueObjects
+{
+    public class MoneyTests
+    {
+        public static IEnumerable<object[]> AmountData = new List<object[]>
+        {
+            new object[] { decimal.Zero, 100.0m },
+            new object[] { 123456.0m, 100.0m },
+            new object[] { 256.9123123m, 256.911231231241234973m },
+            new object[] { 100000000000000000.0m, 1000000000000.0m },
+            new object[] { decimal.Zero, decimal.Zero }
+        };
+
+        public static IEnumerable<object[]> CurrencyData = new List<object[]>
+        {
+            new object[] { Currency.Eur, Currency.Rsd },
+            new object[] { Currency.Rsd, Currency.Eur },
+            new object[] { Currency.Eur, Currency.Eur },
+            new object[] { Currency.Rsd, Currency.Rsd }
+        };
+
+        [Fact]
+        public void MoneyShouldHaveCorrectAmountAndCurrency()
+        {
+            var money = new Money(111.1m, Currency.Eur);
+
+            money.Amount.ShouldBe(111.1m);
+
+            money.Currency.ShouldBe(Currency.Eur);
+        }
+
+        [Fact]
+        public void MoneysShouldBeEqualIfAmountAndCurrencyAreEqual()
+        {
+            var money1 = new Money(111.1m, Currency.Eur);
+            var money2 = new Money(111.1m, Currency.Eur);
+
+            money1.ShouldBe(money2);
+        }
+
+        [Fact]
+        public void MoneysShouldNotBeEqualIfAmountsAreNotEqual()
+        {
+            var money1 = new Money(111.1m, Currency.Eur);
+            var money2 = new Money(222.2m, Currency.Eur);
+
+            money1.ShouldNotBe(money2);
+        }
+
+        [Fact]
+        public void MoneysShouldNotBeEqualIfCurrenciesAreNotEqual()
+        {
+            var money1 = new Money(111.1m, Currency.Eur);
+            var money2 = new Money(111.1m, Currency.Rsd);
+
+            money1.ShouldNotBe(money2);
+        }
+
+        [Theory]
+        [MemberData(nameof(AmountData))]
+        public void MoneyShouldChangeAmount(decimal initialAmount, decimal expectedAmount)
+        {
+            var money = new Money(initialAmount, Currency.Eur);
+
+            money = money.ChangeAmount(expectedAmount);
+
+            money.Amount.ShouldBe(expectedAmount);
+        }
+
+        [Theory]
+        [MemberData(nameof(CurrencyData))]
+        public void MoneyShouldChangeCurrency(Currency initialCurrency, Currency expectedCurrency)
+        {
+            var money = new Money(100.0m, initialCurrency);
+
+            money = money.ChangeCurrency(expectedCurrency);
+
+            money.Currency.ShouldBe(expectedCurrency);
+        }
+    }
+}
